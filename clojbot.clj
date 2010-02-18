d13390bfec4540860ee06afb6b3e0818a73f0bfa
slyphon <slyphon@motionbox.com>
2010-02-18 07:22:17 +0000

    ok, starting to get it, first macro, deal with config merging

--- clojbot.clj
+++ clojbot.clj
@@ -1,25 +1,57 @@
 (ns clojbot
   (:import [java.net Socket])
   (:use [clojure.contrib.except :only (throw-if)]
-        [clojure.contrib.logging]))
+        [clojure.contrib.logging]
+        [clojure.contrib.seq-utils :only (flatten)]))
 
 (def FREENODE "irc.freenode.net")
-(def default-port 6667)
 
-(defstruct message :type :channel :sender :login :hostname :message :target :action)
-(defstruct bot :hostname :port :nick :login :finger)
+(def config-defaults 
+  {:tag ::Config
+   :hostname "localhost" 
+   :port 6667
+   :nick "clojbot"
+   :login "clojbot"
+   :finger "don't finger me!"})
 
-(defn connection-args [bot]
-  (let [hostname (get bot :hostname) port (get bot :port default-port)]
+
+(defstruct message  :tag :type :channel :sender :login :hostname :message :target :action)
+(defstruct config   :tag :hostname :port :nick :login :finger)
+(defstruct bot      :tag :config :socket :listeners :channels)
+
+(defn bot? [x]
+  (and (map? x)
+       (= (x :tag) ::Bot)))
+
+; simple for now
+(defn create-bot [conf]
+  (struct-map bot :tag       ::Bot
+                  :config    conf
+                  :listeners (ref {})
+                  :channels  (ref {})))
+
+(defmacro #^{:private true} struct-hash-map [s hm]
+  `(apply struct-map ~s (flatten (vec ~hm))))
+
+(defn create-config 
+  "creates a config struct for the bot. with no args returns a default config,
+  otherwise expects key/value pairs that will be used to override default values
+  for the config"
+  ([]
+   (struct-hash-map config config-defaults))
+  ([& kvpairs] 
+   (struct-hash-map config (merge config-defaults (apply hash-map kvpairs)))))
+
+(defn- connection-args [conf]
+  (let [hostname (get conf :hostname) port (get conf :port default-port)]
     (throw-if (nil? hostname) IllegalArgumentException "hostname must be set for bot")
     [hostname port]))
+ 
+(defn- default-config []
+  (struct config "localhost" default-port "clojbot" "clojbot" "don't finger me!"))
 
-(defn connect [bot]
-  (let [[hostname port] (connection-args bot)]
+(defn connect [conf]
+  (let [[hostname port] (connection-args conf)]
     (debug (format "hostname: %s, port %d" hostname port))
     (new Socket hostname port)))
-  
-(defn- default-config []
-  (struct bot "localhost" default-port "clojbot" "clojbot" "don't finger me!"))
-
-
+ 
